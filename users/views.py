25103dfaee81d5e8d3ad67df47773eaaeea2a356
pooriaahmadi <pooriababak444@gmail.com>
2022-06-14 18:47:22 -0400

    more production changes

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect, reverse, HttpResponse
 from django.contrib.auth import authenticate, login
 from django.views import View
+from django.conf import settings
 from .models import User
 import requests
 
@@ -9,13 +10,14 @@ class GoogleLanding(View):
     def post(self, request):
         if request.user.is_authenticated:
             return HttpResponseRedirect(reverse("courses:all"))
+        hostname = 'http://localhost:8000' if settings.DEBUG else 'https://pattendance.pooria.tech'
         google_auth_url = "https://accounts.google.com/o/oauth2/v2/auth"
         redirect_uri = reverse("users:google_callback")
         scope = "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile"
         params = {
             'response_type': 'code',
             'client_id': '154280617493-rgn5mgluofrr9ea18l2iunavner30a40.apps.googleusercontent.com',
-            'redirect_uri': f"http://localhost:8000{redirect_uri}",
+            'redirect_uri': f"{hostname}{redirect_uri}",
             'prompt': 'select_account',
             'access_type': 'offline',
             'scope': scope
@@ -33,12 +35,14 @@ class GoogleCallback(View):
         if error:
             return HttpResponse(str(error))
 
+        hostname = 'http://localhost:8000' if settings.DEBUG else 'https://pattendance.pooria.tech'
+
         # Reference: https://developers.google.com/identity/protocols/oauth2/web-server#obtainingaccesstokens
         data = {
             'code': code,
             'client_id': '154280617493-rgn5mgluofrr9ea18l2iunavner30a40.apps.googleusercontent.com',
             'client_secret': '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',
-            'redirect_uri': 'http://localhost:8000/users/google/callback',
+            'redirect_uri': f'{hostname}/users/google/callback',
             'grant_type': 'authorization_code'
         }
         response = requests.post("https://oauth2.googleapis.com/token", data=data)
