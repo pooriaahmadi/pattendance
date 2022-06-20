a3de76f4fd91c735f3326c88390cc6064175bd3a
pooriaahmadi <pooriababak444@gmail.com>
2022-06-20 18:33:02 -0400

    import/export users

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 from users.models import User
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'email')
     search_fields = ('first_name', 'last_name', 'email')
